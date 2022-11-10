7a38ca74b22324cd3958a935b2759b0e2c7fc27d
VictorDuart <victorhd2014@gmail.com>
2022-11-09 23:37:07 -0300

    Creating client form

--- clients-app/src/app/clients/clients-form/clients-form.component.ts
+++ clients-app/src/app/clients/clients-form/clients-form.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Client } from '../clients'
+
 @Component({
   selector: 'app-clients-form',
   templateUrl: './clients-form.component.html',
   styleUrls: ['./clients-form.component.css']
 })
 export class ClientsFormComponent implements OnInit {
+
+  client: Client;
 
   constructor() { }
 
