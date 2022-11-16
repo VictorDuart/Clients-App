7ef1664719771c99566584d69bb6d73367d1460f
VictorDuart <victorhd2014@gmail.com>
2022-11-15 21:35:07 -0300

    Creating new CORS configuration

--- clients-api/src/main/java/io/github/VictorDuart/rest/ClientController.java
+++ clients-api/src/main/java/io/github/VictorDuart/rest/ClientController.java
@@ -12,7 +12,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/clients")
-@CrossOrigin("http://localhost:4200")
 public class ClientController {
 
     private final ClientRepository repository;
